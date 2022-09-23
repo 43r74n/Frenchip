05488ca830db2f898e1e2e0ee98b998755ad8116
43r74n <43r74n@gmail.com>
2022-09-24 00:16:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Frenchip
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-dphlwk)
---
+++ index.ts
@@ -0,0 +1,9 @@
+// Import stylesheets
+import './style.css';
+import './var/br-pt.css';
+import './var/col.css';
+import './var/reset.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+//appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
