f270e80762d827c6ad56c016c675de7b2b4a4041
43r74n <43r74n@gmail.com>
2022-09-30 00:31:23 +0200

    7 files modified, 2 added, 1 deleted

--- index.ts
+++ index.ts
@@ -1,10 +1,8 @@
-// Import stylesheets
 import './var/reset.css';
 import './main.css';
 import './var/br-pt.css';
 import './var/col.css';
-import './var/font-family.css';
+import './var/typeset.css';
 
-// Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
-//appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+//appDiv.innerHTML = `<h1></h1>`;
