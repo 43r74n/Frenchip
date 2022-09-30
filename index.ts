9ae9d2390f025097d38fba0f3aa293fd45f8ddfa
43r74n <43r74n@gmail.com>
2022-09-30 02:38:21 +0200

    3 files modified, 2 added, 1 deleted

--- index.ts
+++ index.ts
@@ -3,7 +3,6 @@ import './main.css';
 import './var/br-pt.css';
 import './var/col.css';
 import './var/typeset.css';
-import './src/site.webmanifest';
 
 const appDiv: HTMLElement = document.getElementById('app');
 //appDiv.innerHTML = `<h1></h1>`;
