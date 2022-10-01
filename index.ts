e2b29ead198e9121340586a75d7e8d16ff5ff856
43r74n <43r74n@gmail.com>
2022-10-01 14:29:19 +0200

    3 files modified, 5 added, 7 deleted

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import './var/reset.css';
 import './main.css';
 import './var/br-pt.css';
-import './var/col.css';
+import './var/colors.css';
 import './var/typeset.css';
 
 const appDiv: HTMLElement = document.getElementById('app');
